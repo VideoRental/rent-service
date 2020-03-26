ec617283b7f67a0a19ac973e4aab2756c3cd4755
batro83 <batro_ska@yahoo.es>
2020-03-26 16:20:03 +0100

    init

---
+++ src/main/java/com/rest/api/rentservice/RentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rest.api.rentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentServiceApplication.class, args);
+	}
+
+}
